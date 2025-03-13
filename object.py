45dd6a6f14ff463659f97e02350018b8a70afc5c
fabiorpfilho <fabio.filho2002@hotmail.com>
2025-03-12 22:43:29 -0300

    Criandos as classes de herança base

--- object.py
+++ object.py
@@ -1,1 +1,20 @@
+import pygame
 
+
+class Object(pygame.sprite.Sprite):
+
+    def __init__(self, x, y, width, height, health=100, attackable=False, solid=False, invicible=False, color=(255, 255, 255)):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.health = health
+        self.attackable = attackable
+        self.invincible = invicible
+        self.solid = solid
+        self.color = color
+
+        self.image = pygame.Surface((self.width, self.height))
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect(topleft=(self.x, self.y))
