b1d6ae4caec5d4aa8c3ca7d58d91686265b8c149
fabiorpfilho <fabio.filho2002@hotmail.com>
2025-03-21 22:33:31 -0300

    Pequenos ajustes

--- src/objects/static_objects/hazard.py
+++ src/objects/static_objects/hazard.py
@@ -1,10 +1,10 @@
 """  Classe para as armadilhas do jogo, que são entidades estáticas que causam dano ou atrapalham o jogador, 
 como espinhos, barris explosivos, ou portas que podem ser atacadas	"""
 
-from objects.entity_with_sprite import EntityWithSprite
+from objects.entity_with_animation import EntityWithAnimation
 
 
-class Hazard(EntityWithSprite):
+class Hazard(EntityWithAnimation):
     def __init__(self, position, size, sprite=(255, 255, 255), solid=True, collide_damage=5, invincible=False,  health=10, attackable=False, attack_speed=0,  damage=10):
         
         super().__init__(position, size, sprite=sprite, solid=solid,
--- src/objects/static_objects/terrain.py
+++ src/objects/static_objects/terrain.py
@@ -1,8 +1,8 @@
 """ Classe para o terreno do jogo, são entidades sólidas que não podem ser atacadas	"""
-from objects.base_object import Object;
+from objects.entity_with_sprite import EntityWithSprite;
 
 
-class Terrain(Object):
+class Terrain(EntityWithSprite):
     def __init__(self, position, size, sprite=(100, 100, 100), texture=None,  friction=1.0, damage=0):
         super().__init__(position, size)
         self.texture = texture  
